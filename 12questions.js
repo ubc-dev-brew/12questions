73225f095e6638cfa0b14d2c51b6ff4c310cd778
Jordan Schalm <jordan.schalm@gmail.com>
2015-10-03 12:53:57 -0700

    Initial commit: skeleton directory structure

---
+++ 12questions.js
@@ -0,0 +1,30 @@
+/*****************************************/
+/* CONSTANTS														 */
+/*****************************************/
+
+var DEFAULT_PORT = 3002;
+
+/*****************************************/
+/* INITIALIZATION												 */
+/*****************************************/
+
+var express = require('express');
+
+var app = express();
+
+var server = http.createServer(app)
+.listen(process.env.PORT || process.argv[2] || DEFAULT_PORT, function() {
+	console.log('TLDRSS v' + tldrss.version + ' running on port: %s', server.address().port);
+});
+
+/*****************************************/
+/* ROUTING															 */
+/*****************************************/
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/views/home.html'));
+});
+
+app.get('/:question', function(req, res) {
+	
+});
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node 12questions.js
